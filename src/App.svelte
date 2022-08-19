c123d0d5d777d5ddfe4910dd92b32d7e3c16d3bf
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-08-19 11:45:20 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  let y;
+</script>
+
+<main>
+  <div class="card h-20 flex flex-col">
+    <p
+      class="absolute  right-1/2 mt-20 z-10"
+      style="top: {y * 1.1}px; transform: rotate( {y * 10}deg );"
+    >
+      {Math.round(y)}
+    </p>
+    <img
+      src={svelteLogo}
+      alt="Svelte"
+      style="transform: rotate( {y / 10}deg );"
+      class=""
+    />
+    <img src={svelteLogo} alt="Svelte" />
+  </div>
+</main>
+<svelte:window bind:scrollY={y} />
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,21 @@
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  count is {count}
+</button>
+
+<style>
+  button {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+  }
+  button:focus {
+    outline: rgb(62, 41, 41) solid 2px;
+  }
+</style>
